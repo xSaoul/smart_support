365caf4f71569f237d0e6593e891ad41ec3a2359
xSaoul <masonneu22@gmail.com>
2025-01-10 01:03:46 -0600

    update guild

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const { ShardClient, ClientOptions } = require('shardclient');
-const client = new ShardClient(new ClientOptions().setGuildCommandsId('1322709752756572190').setDevelopers('439601142528344065'));
+const client = new ShardClient(new ClientOptions().setGuildCommandsId('1120465621554040942').setDevelopers('439601142528344065'));
 client.login();
