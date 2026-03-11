2831f683a53d2b7e361a475fc5b5567517f9abf1
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2026-03-11 21:34:13 +0000

    Add helper's donation links

--- commands/solved.js
+++ commands/solved.js
@@ -1,6 +1,5 @@
 const { CommandBuilder } = require('shardclient');
-const threadModel = require('../db/thread_schema.js')
-
+const threadModel = require('../db/thread_schema.js');
 
 module.exports = new CommandBuilder()
   .setName('solved')
@@ -22,15 +21,15 @@ module.exports = new CommandBuilder()
       console.log('No solved tag found, does it exist?');
       return;
     }
-    
+
     if (!currentTags.includes(solvedTag.id)) {
       const newTags = [solvedTag.id];
       channel.setAppliedTags(newTags);
     }
     await ctx.interaction.reply({ content: `This post has been marked as solved.\n-# Post closed <t:${Date.now().toString().slice(0, -3)}:R>.` });
-    
+
     donationMsg(forumId);
-   
+
     // Sleep for 5 seconds to let OP notice donation message
     await new Promise(r => setTimeout(r, 5000));
 
@@ -39,11 +38,17 @@ module.exports = new CommandBuilder()
 
 async function donationMsg(forumId) {
   const helperLinks = {
-  // OscarSix
-  "211486447369322506": "https://linkme.com"
-};
+    // OscarSix
+    '211486447369322506': 'https://ko-fi.com/viridianlink',
+    // Adiojoe
+    '224057988749459456': 'https://www.buymeacoffee.com/Adiojoe',
+    // Commo
+    '130400218675019777': 'https://buymeacoffee.com/commo',
+    // Tom Tech
+    '357814326662529024': 'https://buymeacoffee.com/tomtech',
+  };
 
-  const threadDb = await threadModel.findOne({threadId: forumId}).exec();
+  const threadDb = await threadModel.findOne({ threadId: forumId }).exec();
 
   let donationMsg = '';
 
@@ -51,11 +56,13 @@ async function donationMsg(forumId) {
     const matchingLinks = threadDb.helperIds.filter(id => helperLinks[id]).map(id => `<@${id}>: ${helperLinks[id]}`);
 
     if (matchingLinks.length > 0) {
-      donationMsg = '\n' + matchingLinks.join('\n')
+      donationMsg = '\n' + matchingLinks.join('\n');
     }
   }
 
   if (donationMsg) {
-    await ctx.interaction.reply({content: `If you feel a helper has been particularly helperful in solving your support ticket please consider donating to them:${donationMsg}`});
+    await ctx.interaction.reply({
+      content: `If you feel a helper has been particularly helperful in solving your support ticket please consider donating to them:${donationMsg}`,
+    });
   }
 }
